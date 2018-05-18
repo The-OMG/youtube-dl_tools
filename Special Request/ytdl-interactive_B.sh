c093bc89a0af3f793f7a5b4349d0f61135d788d2
omg <omg@orionsbelt.space>
2018-05-18 14:55:44 -0800

    giggle.

--- Special Request/ytdl-interactive_B.sh
+++ Special Request/ytdl-interactive_B.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
-#Requirements youtube-dl, ffmpeg, axel
-# Fluff
-echo "Hello $USER. Lets play a game."
-sleep 2
-echo "First things first."
-sleep 1
 
-# User input
-echo "I need a single word that will be your folder name."
-read -p 'Project Name: ' PROJECT
-echo "Now I need your url that I might be pulling from."
-read -p 'URL: ' URL
-sleep 2
-echo "*silently judges*"
-sleep 3
+#Requirements youtube-dl, ffmpeg, axel
 
 # youtube-dl VARS
 YTDLpath="$HOME/$PROJECT"
@@ -22,52 +9,69 @@ ARCHIVE="$YTDLpath/archivedytidlist.txt"
 DOWNLOADER="axel"
 DOWNLOADER_ARGS="-n 15"
 
+function _greeting() {
+  echo "Hello $USER. Lets play a game."
+  sleep 2
+  echo "First things first."
+  sleep 1
+}
+
+function _userINPUT() {
+  echo "I need a single word that will be your folder name."
+  read -p 'Project Name: ' PROJECT
+  echo "Now I need your url that I might be pulling from."
+  read -p 'URL: ' URL
+  sleep 2
+  echo "*silently judges*"
+  sleep 3
+}
+
 # Creates Project folder if you dont have one.
-echo "If ya didnt have a Project folder, ya got one now."
-mkdir $HOME/$PROJECT
-sleep 2s
+function _projectpath() {
+  echo "If ya didnt have a Project folder, ya got one now."
+  mkdir "$YTDLpath"
+  sleep 2s
+}
+
 # Creates inital logfile. | Overwrites existing logfile on every new script run.
-echo "A wild logfile appears!"
-echo 'go go gadget\n' >> $LOGFILE
-echo 'go go gadget\n' >> $ARCHIVE
-echo "$LOGFILE"
-sleep 1s
-echo "$ARCHIVE"
-sleep 5s
+function _logfile() {
+  echo "A wild logfile appears!"
+  echo "go go gadget" >"$LOGFILE"
+  echo "go go gadget" >"$ARCHIVE"
+  echo "$LOGFILE"
+  sleep 1s
+  echo "$ARCHIVE"
+  sleep 5s
+}
 
-#Fluff
-echo "liftoff in"
-sleep 1s
-echo "3"
-sleep 1s
-echo "2"
-sleep 1s
-echo "1"
-sleep 3s
-echo "did you hear that?"
-sleep 3s
-echo "0"
+function _ytdl() {
+  local YTDLARGS=(
+    "--all-subs"
+    "--buffer-size=16k"
+    "--console-title"
+    "--download-archive=$ARCHIVE"
+    "--external-downloader=$DOWNLOADER"
+    "--external-downloader-args=$DOWNLOADER_ARGS"
+    "--fragment-retries=infinite"
+    "--hls-prefer-ffmpeg"
+    "--ignore-errors"
+    "--no-continue"
+    "--no-part"
+    "-o=$YTDLpath/%(uploader)s/%(upload_date)s_%(title)s.%(ext)s"
+    "--playlist-random"
+    "--prefer-ffmpeg"
+    "--retries=infinite"
+    "--verbose"
+    "--write-description"
+    "--write-info-json"
+    "--write-annotations"
+    "--write-all-thumbnails"
+  )
+  youtube-dl "${YTDLARGS[@]}" "$URL" | tee --append "$LOGFILE"
+}
 
-# youtube-dl
-youtube-dl \
---all-subs \
---buffer-size 16k \
---console-title \
---download-archive $ARCHIVE \
---external-downloader $DOWNLOADER \
---external-downloader-args "$DOWNLOADER_ARGS" \
---fragment-retries infinite \
---hls-prefer-ffmpeg \
---ignore-errors \
---no-continue \
---no-part \
--o "$YTDLpath/%(uploader)s/%(upload_date)s_%(title)s.%(ext)s" \
---playlist-random \
---prefer-ffmpeg \
---retries infinite \
---verbose \
---write-description \
---write-info-json \
---write-annotations \
---write-all-thumbnails \
-"$URL" | tee --append $LOGFILE
+_greeting
+_userINPUT
+_projectpath
+_logfile
+_ytdl
